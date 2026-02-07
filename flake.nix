aa18f1633e44da16afed05bca415cb581f64d743
avril.entat-pantagene <avril.entat.pantagene@epita.fr>
2026-02-07 21:59:38 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,27 @@
+# SPDX-License-Identifier: Unlicense
+{
+  inputs = {
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { nixpkgs, ... }@inputs:
+    let
+      eachSystem = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = eachSystem (pkgs: {
+        hello = pkgs.hello;
+      });
+
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Add development dependencies here
+            nodejs_24
+          ];
+        };
+      });
+    };
+}
